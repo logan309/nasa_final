13117f67a8e26e0d36c9df9ebdda54755d8d1edb
Bhoomika luna <shashankgowda1236618@gmail.com>
2025-10-05 19:05:25 +0530

    app.tsx update

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { StarfieldBackground } from "./components/StarfieldBackground";
 import { Navbar } from "./components/Navbar";
 import Home from "./pages/Home";
@@ -14,7 +14,8 @@ import NotFound from "./pages/NotFound";
 const queryClient = new QueryClient();
 
 const App = () => (
-  <BrowserRouter>
+  // HashRouter is used for GitHub Pages deployment to handle routing correctly
+  <HashRouter>
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
@@ -34,7 +35,7 @@ const App = () => (
         </div>
       </TooltipProvider>
     </QueryClientProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default App;
